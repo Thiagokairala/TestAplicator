e17f4122e885588ce99fd005eee0660ce282d48b
Thiagokairala <thiagor.kairala@gmail.com>
2014-09-17 00:40:28 -0300

    included bootstrap and started to make the interface.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,15 @@
 package controllers;
 
-import enums.QuestionStatus;
-import models.Question;
+
 import play.*;
+import play.i18n.Messages;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+		return ok(index.render(Messages.get("index.greeting")));
 	}
+
 }
--- app/controllers/QuestionControl.java
+++ app/controllers/QuestionControl.java
@@ -3,13 +3,18 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import play.*;
+import play.i18n.Messages;
+import play.mvc.*;
+import views.html.*;
 
 public class QuestionControl extends Controller {
-	public static Result index() {
-		return ok(index.render("Your new application is ready."));
-	}
-
 	public static Result registerQuestion() {
 		return TODO;
 	}
+
+	public static Result findTypeOfQuestion() {
+		return ok(findQuestionType.render());
+	}
+
 }
